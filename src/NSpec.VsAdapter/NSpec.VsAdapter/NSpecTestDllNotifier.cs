6ca63541d863489ab89864a93ecf56c5a5ec3e9f
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-01-02 02:33:05 +0100

    Implement NSpecTestDllNotifier

---
+++ src/NSpec.VsAdapter/NSpec.VsAdapter/NSpecTestDllNotifier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSpec.VsAdapter
+{
+    public class NSpecTestDllNotifier : ITestDllNotifier, IDisposable
+    {
+        public NSpecTestDllNotifier(
+            IProjectBuildNotifier projectBuildNotifier, 
+            IProjectBuildConverter projectBuildConverter)
+        {
+            const string notATestDllPath = null;
+
+            var hotPathStream = projectBuildNotifier.BuildStream
+                .Select(buildInfos =>
+                {
+                    IEnumerable<string> dllPaths = buildInfos
+                        .Select(projectBuildConverter.ToTestDllPath)
+                        .Where(path => path != notATestDllPath);
+
+                    return dllPaths;
+                })
+                .Replay(1);
+
+            hotPathStream.Connect().DisposeWith(disposables);
+
+            PathStream = hotPathStream;
+        }
+
+        public IObservable<IEnumerable<string>> PathStream { get; private set; }
+
+        public void Dispose()
+        {
+            disposables.Dispose();
+        }
+
+        readonly CompositeDisposable disposables = new CompositeDisposable();
+    }
+}
---
+++ src/NSpec.VsAdapter/Test/NSpec.VsAdapter.UnitTests/NSpecTestDllNotifier_desc.cs
@@ -0,0 +1,116 @@
+﻿using AutofacContrib.NSubstitute;
+using FluentAssertions;
+using Microsoft.Reactive.Testing;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSpec.VsAdapter;
+using System.Reactive.Subjects;
+
+namespace NSpec.VsAdapter.UnitTests
+{
+    [TestFixture]
+    [Category("NSpecTestDllNotifier")]
+    public class NSpecTestDllNotifier_desc
+    {
+        NSpecTestDllNotifier notifier;
+
+        AutoSubstitute autoSubstitute;
+        Subject<IEnumerable<ProjectBuildInfo>> projectBuildInfoStream;
+        IProjectBuildConverter projectBuildConverter;
+        ITestableObserver<IEnumerable<string>> testDllPathObserver;
+        IDisposable subscription;
+
+        const string notATestDllPath = null;
+
+        [SetUp]
+        public virtual void before_each()
+        {
+            autoSubstitute = new AutoSubstitute();
+
+            var projectBuildNotifier = autoSubstitute.Resolve<IProjectBuildNotifier>();
+            projectBuildInfoStream = new Subject<IEnumerable<ProjectBuildInfo>>();
+            projectBuildNotifier.BuildStream.Returns(projectBuildInfoStream);
+
+            projectBuildConverter = autoSubstitute.Resolve<IProjectBuildConverter>();
+
+            notifier = autoSubstitute.Resolve<NSpecTestDllNotifier>();
+
+            testDllPathObserver = new TestScheduler().CreateObserver<IEnumerable<string>>();
+
+            subscription = notifier.PathStream.Subscribe(testDllPathObserver);
+        }
+
+        [TearDown]
+        public virtual void after_each()
+        {
+            autoSubstitute.Dispose();
+            projectBuildInfoStream.Dispose();
+            notifier.Dispose();
+            subscription.Dispose();
+        }
+
+        [Test]
+        public void it_should_not_notify_when_created()
+        {
+            testDllPathObserver.Messages.Should().BeEmpty();
+        }
+
+        [Test]
+        public void it_should_notify_empty_path_list_when_no_test_is_notified()
+        {
+            var buildInfos = new []
+            {
+                new ProjectBuildInfo(),
+                new ProjectBuildInfo(),
+                new ProjectBuildInfo(),
+            };
+
+            projectBuildConverter.ToTestDllPath(Arg.Any<ProjectBuildInfo>()).Returns(notATestDllPath);
+
+            projectBuildInfoStream.OnNext(buildInfos);
+
+            testDllPathObserver.Messages.Should().HaveCount(1);
+
+            IEnumerable<string> testDllPaths = testDllPathObserver.Messages[0].Value.Value;
+
+            testDllPaths.Should().BeEmpty();
+        }
+
+        [Test]
+        public void it_should_notify_only_test_paths_when_some_test_is_notified()
+        {
+            var testBuildInfo = new ProjectBuildInfo();
+            var anotherTestBuildInfo = new ProjectBuildInfo();
+
+            var buildInfos = new[]
+            {
+                new ProjectBuildInfo(),
+                testBuildInfo,
+                anotherTestBuildInfo,
+            };
+
+            string someTestDllPath = @".\some\dummy\test\library.dll";
+            string anotherTestDllPath = @".\another\dummy\test\library.dll";
+
+            projectBuildConverter.ToTestDllPath(Arg.Any<ProjectBuildInfo>()).Returns(notATestDllPath);
+            projectBuildConverter.ToTestDllPath(testBuildInfo).Returns(someTestDllPath);
+            projectBuildConverter.ToTestDllPath(anotherTestBuildInfo).Returns(anotherTestDllPath);
+
+            projectBuildInfoStream.OnNext(buildInfos);
+
+            testDllPathObserver.Messages.Should().HaveCount(1);
+
+            IEnumerable<string> testDllPaths = testDllPathObserver.Messages[0].Value.Value;
+
+            testDllPaths.Should().HaveCount(2);
+
+            testDllPaths.Should().Contain(someTestDllPath);
+            testDllPaths.Should().Contain(anotherTestDllPath);
+        }
+    }
+}
