5c3c823abfe1b38366d00e7a883c5b7360a5c3cf
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-01-04 01:02:01 +0100

    Implement ISolutionNotifier

--- src/NSpec.VsAdapter/NSpec.VsAdapter/ProjectObservation/SolutionNotifier.cs
+++ src/NSpec.VsAdapter/NSpec.VsAdapter/ProjectObservation/SolutionNotifier.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.VisualStudio.Shell.Interop;
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,27 +16,78 @@ namespace NSpec.VsAdapter.ProjectObservation
         public SolutionNotifier(ISolutionProvider solutionProvider)
         {
             var solution = solutionProvider.Provide();
+
+            var solutionEventStream = Observable.Create<SolutionEventInfo>(observer =>
+                {
+                    uint unregisterToken = VSConstants.VSCOOKIE_NIL;
+                    var solutionEventSink = new SolutionEventSink(observer);
+
+                    // TODO check result and manage failure
+
+                    solution.AdviseSolutionEvents(solutionEventSink, out unregisterToken);
+
+                    Action disposeAction = () =>
+                    {
+                        solution.UnadviseSolutionEvents(unregisterToken);
+                    };
+
+                    return disposeAction;
+                })
+                .Publish() // this will be subscribed multiple times, avoid re-subscription side-effect
+                .RefCount();
+
+            {
+                var hotSolutionOpenedStream = solutionEventStream
+                    .Where(eventInfo => eventInfo.Reason == SolutionEventReason.SolutionOpened)
+                    .Select(_ => new SolutionInfo() { Solution = solution })
+                    .Replay(0);
+
+                hotSolutionOpenedStream.Connect().DisposeWith(disposables);
+
+                SolutionOpenedStream = hotSolutionOpenedStream;
+            }
+
+            {
+                var hotSolutionClosingStream = solutionEventStream
+                    .Where(eventInfo => eventInfo.Reason == SolutionEventReason.SolutionClosing)
+                    .Select(_ => Unit.Default)
+                    .Replay(0);
+
+                hotSolutionClosingStream.Connect().DisposeWith(disposables);
+
+                SolutionClosingStream = hotSolutionClosingStream;
+            }
+
+            {
+                var hotProjectAddedStream = solutionEventStream
+                    .Where(eventInfo => eventInfo.Reason == SolutionEventReason.ProjectAdded)
+                    .Select(eventInfo => new ProjectInfo() { Hierarchy = eventInfo.ProjectHierarchy })
+                    .Replay(0);
+
+                hotProjectAddedStream.Connect().DisposeWith(disposables);
+
+                ProjectAddedStream = hotProjectAddedStream;
+            }
+
+            {
+                var hoProjectRemovingtream = solutionEventStream
+                    .Where(eventInfo => eventInfo.Reason == SolutionEventReason.ProjectRemoving)
+                    .Select(eventInfo => new ProjectInfo() { Hierarchy = eventInfo.ProjectHierarchy })
+                    .Replay(0);
+
+                hoProjectRemovingtream.Connect().DisposeWith(disposables);
+
+                ProjectRemovingtream = hoProjectRemovingtream;
+            }
         }
 
-        public IObservable<SolutionInfo> SolutionOpenedStream
-        {
-            get { throw new NotImplementedException(); }
-        }
+        public IObservable<SolutionInfo> SolutionOpenedStream { get; private set; }
 
-        public IObservable<Unit> SolutionClosingStream
-        {
-            get { throw new NotImplementedException(); }
-        }
+        public IObservable<Unit> SolutionClosingStream { get; private set; }
 
-        public IObservable<ProjectInfo> ProjectAddedStream
-        {
-            get { throw new NotImplementedException(); }
-        }
+        public IObservable<ProjectInfo> ProjectAddedStream { get; private set; }
 
-        public IObservable<ProjectInfo> ProjectRemovingtream
-        {
-            get { throw new NotImplementedException(); }
-        }
+        public IObservable<ProjectInfo> ProjectRemovingtream { get; private set; }
 
         public void Dispose()
         {
@@ -42,5 +95,120 @@ namespace NSpec.VsAdapter.ProjectObservation
         }
 
         readonly CompositeDisposable disposables = new CompositeDisposable();
+
+        class SolutionEventSink : IVsSolutionEvents
+        {
+            public SolutionEventSink(IObserver<SolutionEventInfo> eventObserver)
+            {
+                this.eventObserver = eventObserver;
+            }
+
+            // Solution events
+
+            public int OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+            {
+                eventObserver.OnNext(new SolutionEventInfo()
+                {
+                    Reason = SolutionEventReason.SolutionOpened,
+                });
+
+                return VSConstants.S_OK;
+            }
+
+            public int OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+            {
+                return VSConstants.S_OK;
+            }
+
+            public int OnBeforeCloseSolution(object pUnkReserved)
+            {
+                eventObserver.OnNext(new SolutionEventInfo()
+                {
+                    Reason = SolutionEventReason.SolutionClosing,
+                });
+
+                return VSConstants.S_OK;
+            }
+
+            public int OnAfterCloseSolution(object pUnkReserved)
+            {
+                return VSConstants.S_OK;
+            }
+
+            // Project events
+
+            public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+            {
+                bool solutionOpening = (fAdded == VSConstants.S_FALSE);
+
+                // ignore projects being added while solution is opening
+                if (! solutionOpening)
+                {
+                    eventObserver.OnNext(new SolutionEventInfo()
+                    {
+                        Reason = SolutionEventReason.ProjectAdded,
+                        ProjectHierarchy = pHierarchy,
+                    });
+                }
+
+                return VSConstants.S_OK;
+            }
+
+            public int OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+            {
+                return VSConstants.S_OK;
+            }
+
+            public int OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+            {
+                return VSConstants.S_OK;
+            }
+
+            public int OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+            {
+                return VSConstants.S_OK;
+            }
+
+            public int OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+            {
+                return VSConstants.S_OK;
+            }
+
+            public int OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+            {
+                bool solutionClosing = (fRemoved == VSConstants.S_FALSE);
+
+                // ignore projects being removed while solution is closing
+                if (!solutionClosing)
+                {
+                    eventObserver.OnNext(new SolutionEventInfo()
+                    {
+                        Reason = SolutionEventReason.ProjectRemoving,
+                        ProjectHierarchy = pHierarchy,
+                    });
+                }
+
+                return VSConstants.S_OK;
+            }
+
+            readonly IObserver<SolutionEventInfo> eventObserver;
+        }
+
+        class SolutionEventInfo
+        {
+            public SolutionEventReason Reason;
+
+            public IVsHierarchy ProjectHierarchy;
+        }
+
+        enum SolutionEventReason
+        {
+            Unassigned,
+
+            SolutionClosing,
+            ProjectAdded,
+            SolutionOpened,
+            ProjectRemoving,
+        }
     }
 }
---
+++ src/NSpec.VsAdapter/Test/NSpec.VsAdapter.UnitTests/ProjectObservation/SolutionNotifier_desc.cs
@@ -0,0 +1,189 @@
+﻿using AutofacContrib.NSubstitute;
+using FluentAssertions;
+using Microsoft.Reactive.Testing;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using NSpec.VsAdapter.ProjectObservation;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSpec.VsAdapter.UnitTests.ProjectObservation
+{
+    [TestFixture]
+    [Category("SolutionNotifier")]
+    class SolutionNotifier_desc
+    {
+        SolutionNotifier notifier;
+
+        AutoSubstitute autoSubstitute;
+        IVsSolution someSolution;
+        IVsSolutionEvents solutionEventSink;
+        ITestableObserver<SolutionInfo> solutionOpenedObserver;
+        ITestableObserver<Unit> solutionClosingObserver;
+        ITestableObserver<ProjectInfo> projectAddedObserver;
+        ITestableObserver<ProjectInfo> projectRemovingObserver;
+        CompositeDisposable disposables;
+        IVsHierarchy someHierarchy;
+
+        [SetUp]
+        public virtual void before_each()
+        {
+            autoSubstitute = new AutoSubstitute();
+
+            someSolution = autoSubstitute.Resolve<IVsSolution>();
+
+            uint unregisterToken = VSConstants.VSCOOKIE_NIL;
+            uint dummyToken = 12345;
+
+            someSolution.AdviseSolutionEvents(Arg.Any<IVsSolutionEvents>(), out unregisterToken)
+                .Returns(callInfo =>
+                {
+                    solutionEventSink = callInfo.Arg<IVsSolutionEvents>();
+                    callInfo[1] = dummyToken;
+
+                    return VSConstants.S_OK;
+                });
+
+            var solutionProvider = autoSubstitute.Resolve<ISolutionProvider>();
+            solutionProvider.Provide().Returns(someSolution);
+
+            notifier = autoSubstitute.Resolve<SolutionNotifier>();
+
+            var testScheduler = new TestScheduler();
+
+            solutionOpenedObserver = testScheduler.CreateObserver<SolutionInfo>();
+            solutionClosingObserver = testScheduler.CreateObserver<Unit>();
+            projectAddedObserver = testScheduler.CreateObserver<ProjectInfo>();
+            projectRemovingObserver = testScheduler.CreateObserver<ProjectInfo>();
+
+            disposables = new CompositeDisposable();
+
+            notifier.SolutionOpenedStream.Subscribe(solutionOpenedObserver).DisposeWith(disposables);
+            notifier.SolutionClosingStream.Subscribe(solutionClosingObserver).DisposeWith(disposables);
+            notifier.ProjectAddedStream.Subscribe(projectAddedObserver).DisposeWith(disposables);
+            notifier.ProjectRemovingtream.Subscribe(projectRemovingObserver).DisposeWith(disposables);
+
+            someHierarchy = autoSubstitute.Resolve<IVsHierarchy>();
+        }
+
+        [TearDown]
+        public virtual void after_each()
+        {
+            autoSubstitute.Dispose();
+            notifier.Dispose();
+            disposables.Dispose();
+        }
+
+        [Test]
+        public void it_should_not_notify_when_created()
+        {
+            solutionOpenedObserver.Messages.Should().BeEmpty();
+            solutionClosingObserver.Messages.Should().BeEmpty();
+            projectAddedObserver.Messages.Should().BeEmpty();
+            projectRemovingObserver.Messages.Should().BeEmpty();
+        }
+
+        [Test]
+        public void it_should_notify_when_solution_created()
+        {
+            int created = VSConstants.S_OK;
+
+            solutionEventSink.OnAfterOpenSolution(new Object(), created);
+
+            solutionOpenedObserver.Messages.Should().HaveCount(1);
+            solutionOpenedObserver.Messages.Single().Value.Value.Solution.Should().Be(someSolution);
+
+            solutionClosingObserver.Messages.Should().BeEmpty();
+            projectAddedObserver.Messages.Should().BeEmpty();
+            projectRemovingObserver.Messages.Should().BeEmpty();
+        }
+
+        [Test]
+        public void it_should_not_notify_when_solution_opened()
+        {
+            int created = VSConstants.S_FALSE;
+
+            solutionEventSink.OnAfterOpenSolution(new Object(), created);
+
+            solutionOpenedObserver.Messages.Should().HaveCount(1);
+            solutionOpenedObserver.Messages.Single().Value.Value.Solution.Should().Be(someSolution);
+
+            solutionClosingObserver.Messages.Should().BeEmpty();
+            projectAddedObserver.Messages.Should().BeEmpty();
+            projectRemovingObserver.Messages.Should().BeEmpty();
+        }
+
+        [Test]
+        public void it_should_notify_when_solution_closing()
+        {
+            solutionEventSink.OnBeforeCloseSolution(new Object());
+
+            solutionOpenedObserver.Messages.Should().BeEmpty();
+            solutionClosingObserver.Messages.Should().HaveCount(1);
+            projectAddedObserver.Messages.Should().BeEmpty();
+            projectRemovingObserver.Messages.Should().BeEmpty();
+        }
+
+        [Test]
+        public void it_should_notify_when_project_added()
+        {
+            int added = VSConstants.S_OK;
+
+            solutionEventSink.OnAfterOpenProject(someHierarchy, added);
+
+            solutionOpenedObserver.Messages.Should().BeEmpty();
+            solutionClosingObserver.Messages.Should().BeEmpty();
+            projectAddedObserver.Messages.Should().HaveCount(1);
+            projectAddedObserver.Messages.Single().Value.Value.Hierarchy.Should().Be(someHierarchy);
+
+            projectRemovingObserver.Messages.Should().BeEmpty();
+        }
+
+        [Test]
+        public void it_should_not_notify_when_project_added_while_solution_loads()
+        {
+            int added = VSConstants.S_FALSE;
+
+            solutionEventSink.OnAfterOpenProject(someHierarchy, added);
+
+            solutionOpenedObserver.Messages.Should().BeEmpty();
+            solutionClosingObserver.Messages.Should().BeEmpty();
+            projectAddedObserver.Messages.Should().BeEmpty();
+            projectRemovingObserver.Messages.Should().BeEmpty();
+        }
+
+        [Test]
+        public void it_should_notify_when_project_closing()
+        {
+            int removed = VSConstants.S_OK;
+
+            solutionEventSink.OnBeforeCloseProject(someHierarchy, removed);
+
+            solutionOpenedObserver.Messages.Should().BeEmpty();
+            solutionClosingObserver.Messages.Should().BeEmpty();
+            projectAddedObserver.Messages.Should().BeEmpty();
+            projectRemovingObserver.Messages.Should().HaveCount(1);
+            projectRemovingObserver.Messages.Single().Value.Value.Hierarchy.Should().Be(someHierarchy);
+        }
+
+        [Test]
+        public void it_should_not_notify_when_project_closing_while_solution_closes()
+        {
+            int removed = VSConstants.S_FALSE;
+
+            solutionEventSink.OnBeforeCloseProject(someHierarchy, removed);
+
+            solutionOpenedObserver.Messages.Should().BeEmpty();
+            solutionClosingObserver.Messages.Should().BeEmpty();
+            projectAddedObserver.Messages.Should().BeEmpty();
+            projectRemovingObserver.Messages.Should().BeEmpty();
+        }
+    }
+}
