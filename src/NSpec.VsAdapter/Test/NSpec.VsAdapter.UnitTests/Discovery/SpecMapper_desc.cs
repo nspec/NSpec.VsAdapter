bbdeb30ac06aa08514900e0e20dc44e002e4e2df
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-01-12 01:53:50 +0100

    Add testcase to SpecMapper to target pending examples

--- src/NSpec.VsAdapter/Test/NSpec.VsAdapter.UnitTests/Discovery/SpecMapper_desc.cs
+++ src/NSpec.VsAdapter/Test/NSpec.VsAdapter.UnitTests/Discovery/SpecMapper_desc.cs
@@ -15,18 +15,20 @@ namespace NSpec.VsAdapter.UnitTests.Discovery
 {
     [TestFixture]
     [Category("SpecMapper")]
-    public class SpecMapper_desc
+    public abstract class SpecMapper_desc_base
     {
-        SpecMapper mapper;
+        protected SpecMapper mapper;
 
-        AutoSubstitute autoSubstitute;
-        IDebugInfoProvider debugInfoProvider;
-        Example example;
-        NSpecSpecification expectedSpec;
+        protected AutoSubstitute autoSubstitute;
+        protected IDebugInfoProvider debugInfoProvider;
+        protected Context context;
 
-        const string someAssemblyPath = @".\some\path\to\assembly.dll";
-        const string someSourceCodePath = @".\some\path\to\source\code.cs";
-        const int someLineNumber = 123;
+        // emulates private instance nspec.todo, defined in nspec ancestor
+        protected readonly Action dummyTodo = () => { };
+
+        protected const string someAssemblyPath = @".\some\path\to\assembly.dll";
+        protected const string someSourceCodePath = @".\some\path\to\source\code.cs";
+        protected const int someLineNumber = 123;
 
         [SetUp]
         public virtual void before_each()
@@ -35,36 +37,13 @@ namespace NSpec.VsAdapter.UnitTests.Discovery
 
             var parentContext = new Context("some parent context");
 
-            var context = new Context("some child context");
+            context = new Context("some child context");
             context.Parent = parentContext;
 
-            Action someAction = () => { };
-
-            example = new Example(
-                "some-test-full-name",
-                "tag1 tag2_more tag3",
-                someAction);
-
-            string specClassName = typeof(SpecMapper_desc).ToString();
-            string exampleMethodName = someAction.Method.Name;
-
-            example.Context = context;
-
-            example.Spec = "some specification";
-
-            expectedSpec = new NSpecSpecification()
-            {
-                FullName = example.FullName(),
-                SourceAssembly = someAssemblyPath,
-                SourceFilePath = someSourceCodePath,
-                SourceLineNumber = someLineNumber,
-                Tags = example.Tags.Select(tag => tag.Replace("_", " ")).ToArray(),
-            };
-
-            var navigationData = new DiaNavigationData(someSourceCodePath, someLineNumber, someLineNumber + 4);
-
             debugInfoProvider = autoSubstitute.Resolve<IDebugInfoProvider>();
-            debugInfoProvider.GetNavigationData(specClassName, exampleMethodName).Returns(navigationData);
+
+            var emptyNavigationData = new DiaNavigationData(String.Empty, 0, 0);
+            debugInfoProvider.GetNavigationData(null, null).ReturnsForAnyArgs(emptyNavigationData);
 
             mapper = new SpecMapper(someAssemblyPath, debugInfoProvider);
         }
@@ -74,13 +53,94 @@ namespace NSpec.VsAdapter.UnitTests.Discovery
         {
             autoSubstitute.Dispose();
         }
+    }
+
+    public class SpecMapper_when_example_runs : SpecMapper_desc_base
+    {
+        Example example;
+
+        public override void before_each()
+        {
+            base.before_each();
+
+            Action someAction = () => { };
+
+            example = new Example(
+                "some-test-full-name",
+                "tag1 tag2_more tag3",
+                someAction);
+
+            string specClassName = this.GetType().ToString();
+            string exampleMethodName = someAction.Method.Name;
+
+            example.Context = context;
+
+            example.Spec = "some specification";
+
+            var navigationData = new DiaNavigationData(someSourceCodePath, someLineNumber, someLineNumber + 4);
+
+            debugInfoProvider.GetNavigationData(specClassName, exampleMethodName).Returns(navigationData);
+        }
 
         [Test]
         public void it_should_fill_all_details()
         {
-            var spec = mapper.FromExample(example);
+            var expected = new NSpecSpecification()
+            {
+                FullName = example.FullName(),
+                SourceAssembly = someAssemblyPath,
+                SourceFilePath = someSourceCodePath,
+                SourceLineNumber = someLineNumber,
+                Tags = example.Tags.Select(tag => tag.Replace("_", " ")).ToArray(),
+            };
 
-            spec.ShouldBeEquivalentTo(expectedSpec);
+            var actual = mapper.FromExample(example);
+
+            actual.ShouldBeEquivalentTo(expected);
+        }
+    }
+
+    public class SpecMapper_when_example_is_pending : SpecMapper_desc_base
+    {
+        Example example;
+
+        public override void before_each()
+        {
+            base.before_each();
+
+            example = new Example(
+                "some-test-full-name",
+                "tag1 tag2_more tag3",
+                dummyTodo,
+                pending: true);
+
+            string specClassName = this.GetType().ToString();
+            string exampleMethodName = dummyTodo.Method.Name;
+
+            example.Context = context;
+
+            example.Spec = "some specification";
+
+            var navigationData = new DiaNavigationData(someSourceCodePath, someLineNumber, someLineNumber + 4);
+
+            debugInfoProvider.GetNavigationData(specClassName, exampleMethodName).Returns(navigationData);
+        }
+
+        [Test]
+        public void it_should_lack_source_code_info()
+        {
+            var expected = new NSpecSpecification()
+            {
+                FullName = example.FullName(),
+                SourceAssembly = someAssemblyPath,
+                SourceFilePath = String.Empty,
+                SourceLineNumber = 0,
+                Tags = example.Tags.Select(tag => tag.Replace("_", " ")).ToArray(),
+            };
+
+            var actual = mapper.FromExample(example);
+
+            actual.ShouldBeEquivalentTo(expected);
         }
     }
 }
