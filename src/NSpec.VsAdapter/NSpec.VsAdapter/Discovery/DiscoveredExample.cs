ad0ee82ddecdda8b96026215e84eb7f68f166672
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-03-14 18:06:13 +0100

    Remove unneeded components and comments

--- src/NSpec.VsAdapter/NSpec.VsAdapter/Discovery/DiscoveredExample.cs
+++ src/NSpec.VsAdapter/NSpec.VsAdapter/Discovery/DiscoveredExample.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NSpec.VsAdapter.Discovery
 {
-    [Serializable]  // TODO check if needed
+    [Serializable]
     public class DiscoveredExample
     {
         public string FullName { get; set; }
