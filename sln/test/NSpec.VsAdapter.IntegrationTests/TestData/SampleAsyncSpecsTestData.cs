4a8239006b3730092365e8e908f3930ecf03030f
GiuseppePiscopo <GiuseppePiscopo@users.noreply.github.com>
2017-04-04 13:06:30 +0200

    chore(samples): remove unneeded `using`

--- sln/test/NSpec.VsAdapter.IntegrationTests/TestData/SampleAsyncSpecsTestData.cs
+++ sln/test/NSpec.VsAdapter.IntegrationTests/TestData/SampleAsyncSpecsTestData.cs
@@ -1,9 +1,6 @@
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NSpec.VsAdapter.IntegrationTests.TestData
 {
